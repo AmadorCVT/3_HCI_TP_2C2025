e52530aee37266b46720ba229cd1319d943b5619
Bautista Simone <bsimone@itba.edu.ar>
2025-11-13 20:55:56 -0300

    feat: shopping list item services

---
+++ app/src/main/java/com/example/listi/network/ShoppingListItemService.kt
@@ -0,0 +1,43 @@
+package com.example.listi.network
+
+import com.example.listi.ui.types.*
+import retrofit2.Response
+import retrofit2.http.*
+
+interface ShoppingListItemService {
+
+    // Crear nueva ShoppingListItem
+    @POST("/api/shopping-lists/{id}/items")
+    suspend fun createShoppingListItem(@Body request: CreateShoppingListItemRequest): Response<ShoppingListItemResponse>
+
+    // Obtener lista de categorías
+    @GET("/api/shopping-lists/{id}/items")
+    suspend fun getShoppingListItems(
+        @Query("name") name: String? = null,
+        @Query("page") page: Int = 1,
+        @Query("per_page") perPage: Int = 10,
+        @Query("order") order: String = "ASC",
+        @Query("sort_by") sortBy: String = "createdAt"
+    ): Response<ShoppingListItemListResponse>
+
+    // Obtener una ShoppingListItem por ID
+    @GET("/api/shopping-lists/{id}/items/{item_id}")
+    suspend fun getShoppingListItemById(@Path("id") id: Int): Response<ShoppingListItemResponse>
+
+    // Actualizar ShoppingListItem
+    @PUT("/api/shopping-lists/{id}/items/{item_id}")
+    suspend fun updateShoppingListItem(
+        @Path("id") id: Int,
+        @Body request: UpdateShoppingListItemRequest
+    ): Response<ShoppingListItemResponse>
+
+    // Eliminar ShoppingListItem
+    @DELETE("/api/shopping-lists/{id}/items/{item_id}")
+    suspend fun deleteShoppingListItem(@Path("id") id: Int): Response<Unit>
+
+    // Actualizar estado de compra de un ShoppingListItem
+    @PATCH("/api/shopping-lists/{id}/items/{item_id}")
+    suspend fun toggleStatusShoppingListItem(
+        @Path("id") id: Int,
+        @Path("item_id") itemId: Int): Response<ShoppingListItemResponse>
+}
--- app/src/main/java/com/example/listi/ui/types/ShoppingListItem.kt
+++ app/src/main/java/com/example/listi/ui/types/ShoppingListItem.kt
@@ -13,3 +13,32 @@ data class ShoppingListItem(
     val product: Product
 )
 
+data class CreateShoppingListItemRequest(
+    val unit: String,
+    val quantity: Int,
+    val productId: Int
+)
+
+data class UpdateShoppingListItemRequest(
+    val unit: String,
+    val quantity: Int,
+    val purchased: Boolean,
+    val lastPurchasedAt: String
+)
+
+data class ShoppingListItemResponse(
+    val id: Int,
+    val unit: String,
+    val quantity: Int,
+    val purchased: Boolean,
+    val lastPurchasedAt: String,
+    val createdAt: Date,
+    val updatedAt: Date,
+    val product: Product
+)
+
+data class ShoppingListItemListResponse(
+    val data: List<ShoppingListItem>,
+    val pagination: Pagination
+
+)
